9cb01e558d465e881f97a0467de0d8de48ebd66b
ziaul-hasan-shaon <ziaul@devsnest.net>
2024-02-26 11:29:45 +0600

    start the mission

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Hellow DevsNest</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Layout from "../Layout";
+import Home from "../Pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            }
+        ]
+    }
+])
+
+export default router;
